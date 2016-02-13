685a90c39ccd0e7540377278af692784295fe846
Luna <malalunapg@gmail.com>
2016-02-13 17:30:35 -0500

    Start Menu with buttons

---
+++ Katamiam/Assets/ButtonClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	public void ClickStart () {
+		Debug.Log ("Chu clique !");
+	
+	}
+	
+	// Update is called once per frame
+	public void ClickDeux (string msg) {
+		Debug.Log (msg);
+	
+	}
+}
